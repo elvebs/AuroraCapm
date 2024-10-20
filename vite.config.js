71cfb6d67657e83624168efec1a03f86cd6a5e4c
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-20 15:12:50 +0200

    published

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/AuroraCapm/', // Set this to your repository name
   build: {
     rollupOptions: {
       input: {
