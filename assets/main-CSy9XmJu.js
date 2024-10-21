66a8f6eb9fc777d91bb2b71bd63134b96e34c6f6
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-21 13:14:50 +0200

    Updates

--- assets/main-CSy9XmJu.js
+++ assets/main-CSy9XmJu.js
@@ -1,4 +1,4 @@
-import{c as l,a as i}from"./footer-D5cyop7Q.js";document.addEventListener("DOMContentLoaded",()=>{const e=l();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
+import{c as l,a as i}from"./footer-6nR9eHzH.js";document.addEventListener("DOMContentLoaded",()=>{const e=l();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
   <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
   <!-- Background Image -->
   <img src="./images/DSCF6115.JPG" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
