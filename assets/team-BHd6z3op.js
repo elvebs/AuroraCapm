49c03da3514d99927dc900c6f090897731e64110
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-21 19:09:33 +0200

    Updates

--- assets/team-BHd6z3op.js
+++ assets/team-BHd6z3op.js
@@ -7,7 +7,7 @@ import{c as t,a as s}from"./footer-2GrAZIOX.js";document.addEventListener("DOMCo
     </div>
     <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <li>
-        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/LeoG.jpeg" alt="">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Leonardo Mario Di Gennaro</h3>
         <p class="text-base leading-7 text-gray-600">Master in Finance</p>
         <ul role="list" class="mt-6 flex gap-x-6">
