4a1c5388b0a6dadecb55768bbd061fbc2964a959
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-21 13:17:00 +0200

    Updates

--- assets/contacts-BZO8-t21.js
+++ assets/contacts-BZO8-t21.js
@@ -1,4 +1,4 @@
-import{c as a,a as l}from"./footer-6nR9eHzH.js";document.addEventListener("DOMContentLoaded",()=>{const e=a();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
+import{c as a,a as l}from"./footer-DOIc-P5O.js";document.addEventListener("DOMContentLoaded",()=>{const e=a();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
     <div class="relative isolate bg-white">
       <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <!-- Right Section -->
