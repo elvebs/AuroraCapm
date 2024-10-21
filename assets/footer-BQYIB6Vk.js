308603d3f8bb46eb35f70d7121fed8234c2668ff
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-21 12:59:04 +0200

    Updates

--- assets/footer-BQYIB6Vk.js
+++ assets/footer-BQYIB6Vk.js
@@ -29,7 +29,7 @@
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="">
+            <img class="h-8 w-auto" src="./images/Alogo.png" alt="">
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" id="close-menu-button">
             <span class="sr-only">Close menu</span>
--- assets/main-KogiTC5o.js
+++ assets/main-KogiTC5o.js
@@ -1,4 +1,4 @@
-import{c as l,a as i}from"./footer-C_44YqyG.js";document.addEventListener("DOMContentLoaded",()=>{const e=l();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
+import{c as l,a as i}from"./footer-BQYIB6Vk.js";document.addEventListener("DOMContentLoaded",()=>{const e=l();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
   <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
   <!-- Background Image -->
   <img src="./images/DSCF6115.JPG" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
