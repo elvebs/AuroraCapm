ddbec639a42ce221eb01d608ef440a4e3b522ee8
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-23 09:41:43 +0200

    Updates

--- assets/portfolio-V8s8kUlN.js
+++ assets/portfolio-V8s8kUlN.js
@@ -21,7 +21,7 @@ import{c as o,a as r}from"./footer-PiBz2C01.js";document.addEventListener("DOMCo
   <div class="relative px-6 py-12 sm:px-6 sm:py-16 lg:px-8">
     <div class="mx-auto max-w-7xl">
       <h2 class="text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl text-center" data-aos="fade-up">
-        Current
+        Current Portfolio Breakdown
       </h2>
 
       <!-- Companies Grid -->
--- assets/team-Dq0dNvG-.js
+++ assets/team-Dq0dNvG-.js
@@ -12,7 +12,7 @@ import{c as t,a as s}from"./footer-PiBz2C01.js";document.addEventListener("DOMCo
         <p class="text-base leading-7 text-gray-600">Risk Analyst</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href=https://www.linkedin.com/in/leonardo-mario-di-gennaro-57827522a/" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -30,7 +30,7 @@ import{c as t,a as s}from"./footer-PiBz2C01.js";document.addEventListener("DOMCo
         <p class="text-base leading-7 text-gray-600">Software Developer</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="linkedin.com/in/sara-bratli-elvebakk-642113193" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -46,7 +46,7 @@ import{c as t,a as s}from"./footer-PiBz2C01.js";document.addEventListener("DOMCo
         <p class="text-base leading-7 text-gray-600">Portfolio Manager</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://www.linkedin.com/in/nicolafochi/" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -78,7 +78,7 @@ import{c as t,a as s}from"./footer-PiBz2C01.js";document.addEventListener("DOMCo
         <p class="text-base leading-7 text-gray-600">Quantative Analyst</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://www.linkedin.com/in/alessandro-z-245111175/" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
