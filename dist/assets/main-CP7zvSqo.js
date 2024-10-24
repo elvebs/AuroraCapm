e1f4779ab679836ebaf533bb7cd397d48ea7bc29
elvebs <145670287+elvebs@users.noreply.github.com>
2024-10-24 09:18:40 +0200

    Updated the linkdin URL & portfolio page

---
+++ dist/assets/main-CP7zvSqo.js
@@ -0,0 +1,148 @@
+import{c as l,a as i}from"./footer-PiBz2C01.js";document.addEventListener("DOMContentLoaded",()=>{const e=l();document.body.insertBefore(e,document.body.firstChild);const t=document.getElementById("app");t.innerHTML=`
+  <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+  <!-- Background Image -->
+  <img src="./images/DSCF6115.JPG" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+  
+  <!-- Title and Description Container -->
+  <div class="relative mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center">
+      <!-- Big Title -->
+      <h1 class="mt-2 text-6xl font-bold tracking-tight text-white sm:text-7xl">ABOUT</h1>
+    </div>
+  </div>
+  
+<!-- Plain Background Section for Additional Information -->
+  <div class="relative mx-auto max-w-7xl px-6 lg:px-8">
+  <br>
+ 
+    <p class="text-lg leading-8 text-gray-300 text-center">
+      Founded in 2024, Aurora Capital Management is dedicated to identifying <br> 
+      and nurturing high-potential investment opportunities across various sectors.
+    </p>
+    </p>
+  </div>
+</div>
+
+<!-- SECOND SECTION -->
+<div class="overflow-hidden bg-white py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:flex lg:px-8 lg:items-start">
+    
+    <!-- Left Section: Text Content -->
+    <div class="relative lg:w-full lg:max-w-lg lg:pb-8 flex flex-col">
+      
+      <!-- Text Content -->
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mt-4"> Aurora Capital Management </h2>
+      <p class="mt-4 text-2xl leading-8 text-gray-900">is a purpose-driven asset management firm dedicated to addressing global challenges through strategic investments</p>
+      <p class="mt-6 text-xl leading-8 text-gray-600">We believe that the world's complexities present opportunities for growth and positive impact.</p>
+      <p class="mt-6 text-xl leading-8 text-gray-600">Aurora Capital Management is a dynamic asset management firm based in Lugano, Switzerland. We bring a fresh perspective to the world of asset management, focusing on identifying and supporting innovative companies that are making a positive impact.</p>
+      
+      <!-- Button and AlogoN Image -->
+      <div class="mt-10 flex items-center">
+        <img src="./images/AlogoN.png" alt="Large Image" class="h-[120px] w-[120px] object-cover mr-4">
+        <a href="./team" class="rounded-md bg-[#355172] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#355172] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Join our team <span aria-hidden="true">&rarr;</span>
+        </a>
+      </div>
+    </div>
+
+    <!-- Right Section: Two Group Images aligned vertically (stacked) -->
+    <div class="flex-shrink-0 lg:ml-12 lg:self-start flex flex-col space-y-4">
+      <img src="./images/GrouP2.JPG" alt="Group Image 1" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover">
+      <img src="./images/GrouP.png" alt="Group Image 2" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover">
+    </div>
+    
+  </div>
+</div>
+
+  <div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our team</h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</p>
+    </div>
+    <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <li>
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/LeoG.jpeg" alt="">
+        <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Leonardo Mario Di Gennaro</h3>
+        <p class="text-base leading-7 text-gray-600">Risk Analyst</p>
+        <ul role="list" class="mt-6 flex gap-x-6">
+          <li>
+            <a href="https://it.linkedin.com/in/leonardo-mario-di-gennaro-57827522a" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <!-- More people... -->
+      
+      <li>
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/ptsara-1.png" alt="" style="object-position: center;">
+        <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Sara Elvebakk</h3>
+        <p class="text-base leading-7 text-gray-600">Software Developer</p>
+        <ul role="list" class="mt-6 flex gap-x-6">
+          <li>
+            <a href="https://www.linkedin.com/in/sara-bratli-elvebakk-642113193" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </li>
+      
+      <li>
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/NicP.png" alt="">
+        <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Nicola Fochi</h3>
+        <p class="text-base leading-7 text-gray-600">Portfolio Manager</p>
+        <ul role="list" class="mt-6 flex gap-x-6">
+          <li>
+            <a href="https://www.linkedin.com/in/nicola-fochi-563232269/" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </li>
+      
+      <li>
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/ptluce.jpg" alt="" style="object-position: center top;">
+        <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Yi Lu</h3>
+        <p class="text-base leading-7 text-gray-600">Data Engineer</p>
+        <ul role="list" class="mt-6 flex gap-x-6">
+          <li>
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </li>
+      
+      <li>
+        <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/AleP.png" alt="">
+        <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Alessandro Zanichelli</h3>
+        <p class="text-base leading-7 text-gray-600">Quantative Analyst</p>
+        <ul role="list" class="mt-6 flex gap-x-6">
+          <li>
+            <a href="https://ch.linkedin.com/in/alessandro-z-245111175" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+  `;const a=i();document.body.appendChild(a)});
--- src/team.js
+++ src/team.js
@@ -21,10 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
       <li>
         <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/LeoG.jpeg" alt="">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Leonardo Mario Di Gennaro</h3>
-        <p class="text-base leading-7 text-gray-600">Master in Finance</p>
+        <p class="text-base leading-7 text-gray-600">Risk Analyst</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://linkedin.com/in/leonardo-mario-di-gennaro-57827522a" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -39,10 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
       <li>
         <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/ptsara-1.png" alt="" style="object-position: center;">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Sara Elvebakk</h3>
-        <p class="text-base leading-7 text-gray-600">Master of Science in Financial Technology and Computing</p>
+        <p class="text-base leading-7 text-gray-600">Software Developer</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://www.linkedin.com/in/sara-bratli-elvebakk-642113193" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -55,10 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
       <li>
         <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/NicP.png" alt="">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Nicola Fochi</h3>
-        <p class="text-base leading-7 text-gray-600">Master in Finance</p>
+        <p class="text-base leading-7 text-gray-600">Portfolio Manager</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://www.linkedin.com/in/nicola-fochi-563232269/" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
       <li>
         <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/ptluce.jpg" alt="" style="object-position: center top;">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Yi Lu</h3>
-        <p class="text-base leading-7 text-gray-600">Master of Science in Financial Technology and Computing</p>
+        <p class="text-base leading-7 text-gray-600">Data Engineer</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
             <a href="#" class="text-gray-400 hover:text-gray-500">
@@ -87,10 +87,10 @@ document.addEventListener('DOMContentLoaded', () => {
       <li>
         <img class="aspect-[2/2] w-full rounded-2xl object-cover" src="./images/AleP.png" alt="">
         <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">Alessandro Zanichelli</h3>
-        <p class="text-base leading-7 text-gray-600">Master in Finance</p>
+        <p class="text-base leading-7 text-gray-600">Quantative Analyst</p>
         <ul role="list" class="mt-6 flex gap-x-6">
           <li>
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="https://linkedin.com/in/alessandro-z-245111175" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">LinkedIn</span>
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
